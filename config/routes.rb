859b5d91ab239b0de03cd5c65ddb0a15c9d71fa6
takeyukigoto <62117691+takeyukigoto@users.noreply.github.com>
2020-07-07 09:28:30 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'contact#index'
+  root to: 'kojin#index'
+
+  resources :top, only: :index
 
   resources :guide, only: :index
+
+  resources :reason, only: :index
 end
