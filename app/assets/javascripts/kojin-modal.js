3f56bd8a05cdf3cb5556ee9a0b2034fb5c7d5cea
takeyukigoto <62117691+takeyukigoto@users.noreply.github.com>
2020-07-08 08:56:00 +0900

    add

---
+++ app/assets/javascripts/kojin-modal.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('#kojinmousikomi-modal').click(function() {
+    $('#kojin-modal').fadeIn();
+  });
+  
+  $('.kojin-close-modal').click(function() {
+    $('#kojin-modal').fadeOut();
+     
+  });
+  
+  $('#kojin-modal-btn').click(function() {
+    $('#kojin-modal').fadeOut();
+
+});
+
+});
