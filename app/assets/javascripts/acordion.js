80e8de6974dcd2ea529c305074b77ddc3e33559b
takeyukigoto <62117691+takeyukigoto@users.noreply.github.com>
2020-08-21 07:23:29 +0900

    add

--- app/assets/javascripts/acordion.js
+++ app/assets/javascripts/acordion.js
@@ -3,18 +3,18 @@ $('.faq-list-item').click(function() {
   var $answer = $(this).find('.answer');
   if($answer.hasClass('open')) { 
     $answer.removeClass('open');
-    // slideUpメソッドを用いて、$answerを隠してください
+   
     $answer.slideUp();
 
-    // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+    
     $(this).find('span').text('ANSWER CLICK!');
     
   } else {
     $answer.addClass('open'); 
-    // slideDownメソッドを用いて、$answerを表示してください
+    
     $answer.slideDown();
     
-    // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+    
     $(this).find('span').text('-');
     
   }
